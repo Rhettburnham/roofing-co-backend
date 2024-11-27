090429e3b527e658758d21e017c91e32ed6138d7
Rhettburnham <Rhett.burnham@emory.edu>
2024-11-27 16:24:18 -0500

    fixed

--- app.js
+++ app.js
@@ -89,3 +89,4 @@ app.post("/submit-booking", async (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
